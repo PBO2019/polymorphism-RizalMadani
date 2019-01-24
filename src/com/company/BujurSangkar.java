daa13eac1766162785006a15b9332ca1d01670d5
Rizal Madani <46768199+RizalMadani@users.noreply.github.com>
2019-01-24 09:12:05 +0700

    Kelilikng bujursangkar

---
+++ src/com/company/BujurSangkar.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class BujurSangkar {
+
+    public static void main(String[]args){
+        int sisi=15;
+        int keliling=sisi+sisi+sisi+sisi;
+
+        System.out.println("Sisi persegi : "+sisi);
+        System.out.println("Kelliling : "+keliling);
+    }
+}
