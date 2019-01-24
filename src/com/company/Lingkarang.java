f8938dbd3a54075d57cf3383a2218ee482e5657a
Rizal Madani <46768199+RizalMadani@users.noreply.github.com>
2019-01-24 09:29:34 +0700

    Kelilikng bujursangkar

---
+++ src/com/company/Lingkarang.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Lingkarang {
+    public static void main(String[]args){
+        int jari=35;
+        double phi=3.14;
+        double keliling=2*phi*jari;
+
+        System.out.println("Jari-jari lingkaran : "+jari);
+        System.out.println("Keliling        : "+keliling);
+    }
+}
